203b7c734460b565d0f71955ed062cb3fc92c2a6
sarthisarthak <140862504+sarthisarthak@users.noreply.github.com>
2024-08-14 01:17:34 +0530

    pricing_done

--- src/landing_page/pricing/Brokerage.js
+++ src/landing_page/pricing/Brokerage.js
@@ -1,7 +1,52 @@
 import React from "react";
 
 const Brokerage = () => {
-  return <div>Brokerage</div>;
+  return (
+    <div className="container">
+      <div className="row p-5 mt-5 text-center">
+        <div className="col-8 p-4">
+          <a href="/" style={{ textDecoration: "none" }}>
+            <h3
+              style={{ textAlign: "left", marginLeft: "60px" }}
+              className="fs-5 mb-5"
+            >
+              Brokerage calculator
+            </h3>
+            <ul
+              className="text-muted fs-8 text-small"
+              style={{ textAlign: "left", lineHeight: "1.8" }}
+            >
+              <li>
+                Call & Trade and RMS auto-squareoff: Additional charges of ₹50 +
+                GST per order.
+              </li>
+              <li>Digital contract notes will be sent via e-mail.</li>
+              <li>
+                Physical copies of contract notes, if required, shall be charged
+                ₹20 per contract note. Courier charges apply.
+              </li>
+              <li>
+                For NRI account (non-PIS), 0.5% or ₹100 per executed order for
+                equity (whichever is lower).
+              </li>
+              <li>For NRI account (PIS), 0.5% or ₹200 per executed order</li>
+              for equity (whichever is lower).
+              <li>
+                If the account is in debit balance, any order placed will be
+                charged ₹40 per executed order instead of ₹20 per executed
+                order.
+              </li>
+            </ul>
+          </a>
+        </div>
+        <div className="col-4 p-4">
+          <a href="/" style={{ textDecoration: "none" }}>
+            <h3 className="fs-5 mb-5">List of charges</h3>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Brokerage;
--- src/landing_page/pricing/Hero.js
+++ src/landing_page/pricing/Hero.js
@@ -1,7 +1,58 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="container ">
+      <div
+        className="row border-bottom p-5 mt-5 mb-5 fs-6 text-center"
+        style={{ lineHeight: "1.9" }}
+      >
+        <h1>Pricing</h1>
+        <p className="text-muted mb-5">
+          Free equity investments and flat ₹20 traday and F&O trades
+        </p>
+      </div>
+      <div className="row mt-5 p-5 border-bottom">
+        <div className="col-4 p-3 text-center">
+          <img
+            src="media/images/pricingEquity.svg"
+            alt="equity"
+            style={{ width: "70%" }}
+          />
+          <h1 className="fs-2 mb-3">Free equity delivery</h1>
+          <p className="text-muted">
+            All equity delivery investments (NSE, BSE), are absolutely free — ₹
+            0 brokerage.
+          </p>
+        </div>
+        <div className="col-4 p-3 text-center">
+          <img
+            src="media/images/intradayTrades.svg"
+            alt="equity"
+            style={{ width: "70%" }}
+          />
+          <h1 className="fs-2 mb-3">Intraday and F&O trades</h1>
+          <p className="text-muted">
+            Flat ₹ 20 or 0.03% (whichever is lower) per executed order on
+            intraday trades across equity, currency, and commodity trades. Flat
+            ₹20 on all option trades.
+          </p>
+        </div>
+        <div className="col-4 p-3 text-center">
+          <img
+            src="media/images/pricingEquity.svg"
+            alt="equity"
+            style={{ width: "70%" }}
+          />
+          <h1 className="fs-2 mb-3">Free direct MF</h1>
+          <p className="text-muted">
+            All direct mutual fund investments are absolutely free — ₹ 0
+            commissions & DP charges.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
